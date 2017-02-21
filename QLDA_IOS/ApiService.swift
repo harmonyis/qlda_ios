ac5605cd07d70c64f4c04ab1f28f27db43c8e679
datlh <ledat010994@gmail.com>
2017-02-21 10:47:33 +0700

    sssd

---
+++ QLDA_IOS/ApiService.swift
@@ -0,0 +1,100 @@
+//
+//  ApiService.swift
+//  DemoSwift
+//
+//  Created by dungnn on 2/15/17.
+//  Copyright © 2017 dungnn. All rights reserved.
+//
+
+import Foundation
+
+class ApiService {
+    init () {
+        
+    }
+    //typealias MethodHandler1 = (dataResult : String)  -> Void
+    func Get (url : String,callback:@escaping (_ dataResult : Data) -> Void,
+              errorCallBack:@escaping (_ error : Error) -> Void) -> Void {
+        //var resultJson : String
+        //resultJson = ""
+        
+        //var message :String  = "demo demo"
+        
+        let config = URLSessionConfiguration.default // Session Configuration
+        let session = URLSession(configuration: config) // Load configuration into Session
+        let url = URL(string: url)!
+        
+        //let postString = params
+        //let theRequest = NSMutableURLRequest(url: url as URL		)
+        //theRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        //theRequest.httpMethod = "GET"
+        //theRequest.httpBody = postString.data(using: .utf8)
+        let task = session.dataTask(with: url, completionHandler: {
+            (data, response, error) in
+            
+            if error != nil {
+                //resultJson = error!.localizedDescription
+                errorCallBack(error!)
+            } else {
+                //if let data = data, let result = String(data: data, encoding: String.Encoding.utf8) {
+                if let data = data {
+                    //resultJson = result
+                    callback(data)
+                }
+                
+            }
+            
+        })
+        
+        task.resume()
+        
+        
+        //return resultJson
+    }
+    
+    func Post (url : String, params: String,callback:@escaping (_ dataResult : Data) -> Void,
+               errorCallBack:@escaping (_ error : Error) -> Void) -> Void {
+        //var message :String  = ""
+        
+        let config = URLSessionConfiguration.default // Session Configuration
+        let session = URLSession(configuration: config) // Load configuration into Session
+        let url = URL(string: url)!
+        
+        let postString = params
+        let theRequest = NSMutableURLRequest(url: url as URL)
+        theRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        theRequest.httpMethod = "POST"
+        theRequest.httpBody = postString.data(using: .utf8)
+        let task = session.dataTask(with: theRequest as URLRequest, completionHandler: {
+            (data, response, error) in
+            
+            if error != nil {
+                
+                //message = error!.localizedDescription
+                errorCallBack(error!)
+            } else {
+                
+                //if let data = data, let result = String(data: data, encoding: String.Encoding.utf8) {
+                if let data = data {
+                    //message = result
+                    callback(data)
+                }
+            }
+            
+        })
+        
+        task.resume()
+    }
+    
+    func Put (url : String, params: String) -> String {
+        var result : String
+        result = ""
+        return result
+    }
+    
+    func Delete (url : String, params: String) -> String {
+        var result : String
+        result = ""
+        return result
+    }
+}
