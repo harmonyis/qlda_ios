5147c3d898d43e8edbb36651117490db8b89d743
datlh <ledat010994@gmail.com>
2017-02-21 17:16:57 +0700

    thời gian thực

--- QLDA_IOS/Chat_VC.swift
+++ QLDA_IOS/Chat_VC.swift
@@ -9,13 +9,20 @@
 import UIKit
 
 class Chat_VC: UIViewController {
-
+    @IBOutlet weak var imgData: UIImageView!
     @IBOutlet weak var lblChat: UILabel!
+    
     var passData : String!
+    var pictureUrl : String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        lblChat.text = passData
-        // Do any additional setup after loading the view.
+        lblChat.text = pictureUrl
+        
+        //if let checkedUrl = URL(string: pictureUrl) {
+            //imgData.contentMode = .scaleAspectFit
+            //downloadImage(url: checkedUrl)
+        //}
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,14 +31,22 @@ class Chat_VC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _  response: URLResponse?, _ error: Error?) -> Void) {
+        URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            completion(data, response, error)
+            }.resume()
     }
-    */
 
+    func downloadImage(url: URL) {
+        print("Download Started")
+        getDataFromUrl(url: url) { (data, response, error)  in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() { () -> Void in
+                self.imgData.image = UIImage(data: data)
+            }
+        }
+    }
 }
