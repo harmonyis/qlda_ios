dcf1759aaaba0093024c037cdad5ff62a4a4ff29
datlh <ledat010994@gmail.com>
2017-02-22 11:36:08 +0700

    swiftR

---
+++ SwiftR-master/SwiftR iOS Demo/DemoViewController.swift
@@ -0,0 +1,157 @@
+//
+//  DemoViewController.swift
+//  SwiftR
+//
+//  Created by Adam Hartford on 5/26/16.
+//  Copyright © 2016 Adam Hartford. All rights reserved.
+//
+
+import UIKit
+import SwiftR
+
+class DemoViewController: UIViewController {
+    
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var chatTextView: UITextView!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var startButton: UIBarButtonItem!
+    
+    var chatHub: Hub!
+    var connection: SignalR!
+    var name: String!
+    var userID: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        connection = SignalR("http://harmonysoft.vn:8089/QLDA_Services/")
+        //connection.useWKWebView = true
+        connection.signalRVersion = .v2_2_0
+        
+        chatHub = Hub("chatHub")
+        chatHub.on("broadcastMessage") { [weak self] args in
+            if let name = args?[0] as? String, let message = args?[1] as? String, let text = self?.chatTextView.text {
+                self?.chatTextView.text = "\(text)\n\n\(name): \(message)"
+            }
+        }
+        chatHub.on("onConnected") { [weak self] args in
+
+            if let userID = args?[0] as? Int, let message = args?[1] as? String, let text = self?.chatTextView.text {
+                self?.chatTextView.text = "\(text)\n\n\(userID): \(message)"
+            }
+        }
+        connection.addHub(chatHub)
+        
+         // SignalR events
+        
+        connection.starting = { [weak self] in
+            self?.statusLabel.text = "Starting..."
+            self?.startButton.isEnabled = false
+            self?.sendButton.isEnabled = false
+        }
+
+        connection.reconnecting = { [weak self] in
+            self?.statusLabel.text = "Reconnecting..."
+            self?.startButton.isEnabled = false
+            self?.sendButton.isEnabled = false
+        }
+
+        connection.connected = { [weak self] in
+            print("Connection ID: \(self!.connection.connectionID!)")
+            self?.statusLabel.text = "Connected"
+            self?.startButton.isEnabled = true
+            self?.startButton.title = "Stop"
+            self?.sendButton.isEnabled = true
+        }
+
+        connection.reconnected = { [weak self] in
+            self?.statusLabel.text = "Reconnected. Connection ID: \(self!.connection.connectionID!)"
+            self?.startButton.isEnabled = true
+            self?.startButton.title = "Stop"
+            self?.sendButton.isEnabled = true
+        }
+
+        connection.disconnected = { [weak self] in
+            self?.statusLabel.text = "Disconnected"
+            self?.startButton.isEnabled = true
+            self?.startButton.title = "Start"
+            self?.sendButton.isEnabled = false
+        }
+
+        connection.connectionSlow = { print("Connection slow...") }
+
+        connection.error = { [weak self] error in
+            print("Error: \(error)")
+
+            // Here's an example of how to automatically reconnect after a timeout.
+            //
+            // For example, on the device, if the app is in the background long enough
+            // for the SignalR connection to time out, you'll get disconnected/error
+            // notifications when the app becomes active again.
+            
+            if let source = error?["source"] as? String, source == "TimeoutException" {
+                print("Connection timed out. Restarting...")
+                self?.connection.start()
+            }
+        }
+        
+        connection.start()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let alertController = UIAlertController(title: "Name", message: "Please enter your name", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { [weak self] _ in
+            self?.name = alertController.textFields?.first?.text
+            
+            if let name = self?.name , name.isEmpty {
+                self?.name = "Anonymous"
+            }
+            
+            alertController.textFields?.first?.resignFirstResponder()
+        }
+        
+        alertController.addTextField { textField in
+            textField.placeholder = "Your Name"
+        }
+        
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func send(_ sender: AnyObject?) {
+        if let hub = chatHub, let message = messageTextField.text {
+            do {
+                try hub.invoke("send", arguments: [name, message])
+            } catch {
+                print(error)
+            }
+        }
+        messageTextField.resignFirstResponder()
+    }
+    
+    @IBAction func startStop(_ sender: AnyObject?) {
+        if startButton.title == "Start" {
+            connection.start()
+        } else {
+            connection.stop()
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwiftR-master/SwiftR iOS Demo/ViewController.swift
@@ -0,0 +1,166 @@
+//
+//  ViewController.swift
+//  SwiftR iOS Demo
+//
+//  Created by Adam Hartford on 4/16/15.
+//  Copyright (c) 2015 Adam Hartford. All rights reserved.
+//
+
+import UIKit
+import SwiftR
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    var simpleHub: Hub!
+    var complexHub: Hub!
+    
+    var hubConnection: SignalR!
+    var persistentConnection: SignalR!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Make sure myserver.com is mapped to 127.0.0.1 in /etc/hosts
+        // Or change myserver.com to localhost or IP below
+        
+        // Hubs...
+        hubConnection = SignalR("http://harmonysoft.vn:8089/QLDA_Services/")
+        hubConnection.useWKWebView = true
+        hubConnection.transport = .serverSentEvents
+        hubConnection.queryString = ["foo": "bar"]
+        hubConnection.headers = ["X-MyHeader1": "Value1", "X-MyHeader2": "Value2"]
+        
+        // This only works with WKWebView on iOS >= 9
+        // Otherwise, use NSUserDefaults.standardUserDefaults().registerDefaults(["UserAgent": "SwiftR iOS Demo App"])
+        hubConnection.customUserAgent = "SwiftR iOS Demo App"
+            
+        simpleHub = Hub("ChatHub")
+        
+        simpleHub.on("notifySimple") { args in
+            let message = args![0] as! String
+            let detail = args![1] as! String
+            print("Message: \(message)\nDetail: \(detail)\n")
+        }
+        
+        hubConnection.addHub(simpleHub)
+            
+        // SignalR events
+        
+        hubConnection.starting = { [weak self] in
+            print("Starting...")
+            self?.startButton.isEnabled = false
+            self?.startButton.setTitle("Connecting...", for: UIControlState())
+        }
+        
+        hubConnection.reconnecting = { [weak self] in
+            print("Reconnecting...")
+            self?.startButton.isEnabled = false
+            self?.startButton.setTitle("Reconnecting...", for: UIControlState())
+        }
+        
+        hubConnection.connected = { [weak self] in
+            print("Connected. Connection ID: \(self?.hubConnection.connectionID!)")
+            self?.startButton.isEnabled = true
+            self?.startButton.setTitle("Stop", for: UIControlState())
+        }
+        
+        hubConnection.reconnected = { [weak self] in
+            print("Reconnected. Connection ID: \(self?.hubConnection.connectionID!)")
+            self?.startButton.isEnabled = true
+            self?.startButton.setTitle("Stop", for: UIControlState())
+        }
+        
+        hubConnection.disconnected = { [weak self] in
+            print("Disconnected.")
+            self?.startButton.isEnabled = true
+            self?.startButton.setTitle("Start", for: UIControlState())
+        }
+        
+        hubConnection.connectionSlow = { print("Connection slow...") }
+        
+        hubConnection.error = { [weak self] error in
+            print("Error: \(error)")
+            
+            // Here's an example of how to automatically reconnect after a timeout.
+            //
+            // For example, on the device, if the app is in the background long enough
+            // for the SignalR connection to time out, you'll get disconnected/error
+            // notifications when the app becomes active again.
+            
+            if let source = error?["source"] as? String , source == "TimeoutException" {
+                print("Connection timed out. Restarting...")
+                self?.hubConnection.start()
+            }
+        }
+        
+        hubConnection.start()
+        
+        // Persistent connection...
+        // Uncomment when using persitent connections on your SignalR server
+//        persistentConnection = SignalR("http://myserver.com:5000/echo", connectionType: .persistent)
+//        persistentConnection.received = { data in
+//            print(data!)
+//        }
+//        persistentConnection.connect()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sendSimpleMessage(_ sender: AnyObject?) {
+        do {
+            try simpleHub.invoke("sendSimple", arguments: ["Simple Test", "This is a simple message"])
+        } catch {
+            print(error)
+        }
+        
+        // Or...
+        
+//        simpleHub.invoke("sendSimple", arguments: ["Simple Test", "This is a simple message"]) { (result, error) in
+//            if let e = error {
+//                print("Error: \(e)")
+//            } else {
+//                print("Done!")
+//                if let r = result {
+//                    print("Result: \(r)")
+//                }
+//            }
+//        }
+    }
+    
+    @IBAction func sendComplexMessage(_ sender: AnyObject?) {
+        let message = [
+            "messageId": 1,
+            "message": "Complex Test",
+            "detail": "This is a complex message",
+            "items": ["foo", "bar", "baz"]
+        ] as [String : Any]
+        
+        do {
+            try complexHub.invoke("sendComplex", arguments: [message])
+        } catch {
+            print(error)
+        }
+    }
+    
+    @IBAction func sendData(_ sender: AnyObject?) {
+        persistentConnection.send("Persistent Connection Test")
+    }
+    
+    @IBAction func startStop(_ sender: AnyObject?) {
+        switch hubConnection.state {
+        case .disconnected:
+            hubConnection.start()
+        case .connected:
+            hubConnection.stop()
+        default:
+            break
+        }
+    }
+}
+
