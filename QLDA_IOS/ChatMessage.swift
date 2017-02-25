05b2b9f4009a16c468fef46fedc778dd6660d929
datlh <ledat010994@gmail.com>
2017-02-25 10:48:48 +0700

    load message

---
+++ QLDA_IOS/ChatMessage.swift
@@ -0,0 +1,33 @@
+//
+//  ChatMessage.swift
+//  QLDA_IOS
+//
+//  Created by datlh on 2/25/17.
+//  Copyright © 2017 datlh. All rights reserved.
+//
+
+import Foundation
+
+class ChatMessage{
+    
+    init() {
+        
+        ID = 0
+        IsMe = true
+        Message = ""
+        SenderID = 0
+        Created = Date()
+        SenderName = ""
+        MessageType = 0
+        ContactType = 1
+    }
+    
+    var ID : Int64?
+    var IsMe : Bool?
+    var Message : String?
+    var SenderID : Int?
+    var Created : Date?
+    var SenderName : String?
+    var MessageType : Int?
+    var ContactType : Int?
+}
--- QLDA_IOS/Chat_VC.swift
+++ QLDA_IOS/Chat_VC.swift
@@ -8,18 +8,102 @@
 
 import UIKit
 
-class Chat_VC: UIViewController {
+class Chat_Cell: UITableViewCell{
+    @IBOutlet weak var lblContactName: UILabel!
+    @IBOutlet weak var lblMessage: UILabel!
+}
+
+
+class Chat_VC: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
+    @IBOutlet weak var tblConversation: UITableView!
     var contactID : Int!
     var contactType : Int!
     var service = ApiService()
+    var listMessage : [ChatMessage] = [ChatMessage]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(contactID, contactType)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getMessage()
+        self.tblConversation.allowsSelection = false
+        self.tblConversation.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : Chat_Cell = tableView.dequeueReusableCell(withIdentifier: "cellChat") as! Chat_Cell
+        let msg : ChatMessage = listMessage[indexPath.row]
+        cell.lblContactName.text = msg.SenderName
+        cell.lblMessage.text = msg.Message
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {        
+        return listMessage.count;
+    }
+    
+    func getMessage(){
+        if contactType == 1 {
+            let apiUrl : String = "\(UrlPreFix.Chat.rawValue)/Chat_GetPrivateMessage?senderID=\(ChatHub.userID)&receiverID=\(String(contactID))"
+            print(apiUrl)
+            ApiService.Get(url: apiUrl, callback: callbackGetMsg, errorCallBack: errorGetMsg)
+        }
+        else{
+            
+        }
+    }
+    func callbackGetMsg(data : Data) {
+        let json = try? JSONSerialization.jsonObject(with: data, options: [])
+        if let dic = json as? [[String:Any]] {
+            for item in dic{
+                let msg = ChatMessage()
+                msg.ID =  item["InboxID"] as? Int64
+                msg.Message = item["Message"] as? String
+                msg.MessageType = item["TypeMessage"] as? Int
+                msg.ContactType = item["Type"] as? Int
+                msg.SenderID = item["SenderID"] as? Int
+                if msg.SenderID == ChatHub.userID {
+                    msg.IsMe = true
+                    msg.SenderName = ChatHub.userName
+                }
+                else{
+                    msg.IsMe = false
+                    msg.SenderName = ChatCommon.listContact.filter(){
+                        if(msg.ContactType == $0.TypeOfContact && msg.SenderID == $0.ContactID){
+                            return true
+                        }else{
+                            return false
+                        }
+                        }.first?.Name
+                }
+                
+                msg.Created = Date(jsonDate: item["Created"] as! String)
+                
+                listMessage.append(msg)
+            }
+        }
+        
+        DispatchQueue.main.async() { () -> Void in
+            self.tblConversation.reloadData()
+        }
+    }
+    
+    func errorGetMsg(error : Error) {
+        let message = error.localizedDescription
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
